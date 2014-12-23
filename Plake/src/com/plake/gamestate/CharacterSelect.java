d3a10bf952f67860c71fb6c9fd247c9476da54be
max.graman <max.graman@10.115.41.170>
2014-12-22 23:39:49 -0500

    Added Purple Dragon to all files.

--- Plake/src/com/plake/gamestate/CharacterSelect.java
+++ Plake/src/com/plake/gamestate/CharacterSelect.java
@@ -20,14 +20,14 @@ public class CharacterSelect extends GameState {
 	private Background bg;
 
 	private int currentChoice = 0;
-	private String[] options = { "Green", "Kat", "Blue" };
+	private String[] options = { "Green", "Kat", "Blue", "Purple" };
 
 	private Color versionColor;
 
 	private Font font;
 	private Font fontInfo;
 
-	private BufferedImage left, right, enter, greendrag, kat, bluedrag, /*
+	private BufferedImage left, right, enter, greendrag, kat, bluedrag, purpledrag,/*
 																		 * comingsoon
 																		 * ,
 																		 * soontitle
@@ -62,6 +62,7 @@ public class CharacterSelect extends GameState {
 			greendrag = ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/greendrag.gif"));
 			kat = ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/kat.gif"));
 			bluedrag = ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/bluedrag.gif"));
+			purpledrag = ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/purpledrag.gif"));
 
 			// comingsoon =
 			// ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/comingsoon.gif"));
@@ -89,6 +90,7 @@ public class CharacterSelect extends GameState {
 		g.drawImage(greendrag, 75, 130, null);
 		g.drawImage(kat, 154, 133, null);
 		g.drawImage(bluedrag, 230, 130, null);
+		g.drawImage(purpledrag, 306, 130, null);
 
 		g.setColor(versionColor);
 		g.setFont(fontInfo);
@@ -109,6 +111,9 @@ public class CharacterSelect extends GameState {
 			} else if (i == currentChoice && i == 2) {
 				g.setColor(Color.BLACK);
 				g.drawImage(highlighted, 230, 130, null);
+			} else if (i == currentChoice && i == 3) {
+				g.setColor(Color.BLACK);
+				g.drawImage(highlighted, 306, 130, null);
 			} else {
 				g.setColor(Color.RED);
 			}
@@ -131,6 +136,12 @@ public class CharacterSelect extends GameState {
 		}
 		if (currentChoice == 2) {
 			Player.path = "/Sprites/Player/Bluedrag/playersprites.gif";
+			FireBall.path = "/Sprites/Player/fireball.gif";
+			gsm.setState(1);
+			//JukeBox.stop("menuMusic");
+		}
+		if (currentChoice == 3) {
+			Player.path = "/Sprites/Player/Purpledrag/playersprites.gif";
 			FireBall.path = "/Sprites/Player/fireball.gif";
 			gsm.setState(1);
 			//JukeBox.stop("menuMusic");
