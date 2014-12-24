dd2f36ece0064b8c006c4891803b3662ef47b421
max.graman <max.graman@10.115.41.170>
2014-12-23 19:05:07 -0500

    Removed uneeded .jar(s) in referenced libraries.

--- Plake/src/com/plake/gamestate/CharacterSelect.java
+++ Plake/src/com/plake/gamestate/CharacterSelect.java
@@ -15,34 +15,24 @@ import com.plake.tilemap.Background;
 import com.plake.utils.Keys;
 
 public class CharacterSelect extends GameState {
-
 	private Background bg;
-
 	private int currentChoice = 0;
 	private String[] options = { "Green", "Kat", "Blue", "Purple" };
-
 	private Color versionColor;
-
 	private Font font;
 	private Font fontInfo;
-
-	private BufferedImage left, right, enter, greendrag, kat, bluedrag, purpledrag,/*
-																		 * comingsoon
-																		 * ,
-																		 * soontitle
-																		 * ,
-																		 */title, highlighted;
-
+	private BufferedImage left, right, enter, greendrag, kat, bluedrag,
+			purpledrag,/*
+						 * comingsoon , soontitle ,
+						 */title, highlighted;
 	private HashMap<String, AudioPlayer> sfx;
 
 	public CharacterSelect(GameStateManager gsm) {
 		super(gsm);
-
 		sfx = new HashMap<String, AudioPlayer>();
 		sfx.put("menuselect", new AudioPlayer("/SFX/menuselect.mp3"));
 		sfx.put("menuoption", new AudioPlayer("/SFX/menuoption.mp3"));
 		sfx.put("bump", new AudioPlayer("/SFX/bump.mp3"));
-
 		this.gsm = gsm;
 		try {
 			bg = new Background("/Backgrounds/menubg.gif", 1);
@@ -50,25 +40,29 @@ public class CharacterSelect extends GameState {
 			versionColor = new Color(84, 84, 84);
 			font = new Font("Arial", Font.PLAIN, 12);
 			fontInfo = new Font("Arial", Font.PLAIN, 10);
-
-			title = ImageIO.read(getClass().getResourceAsStream("/Menu/charsel.png"));
-			highlighted = ImageIO.read(getClass().getResourceAsStream("/Menu/highlighted.gif"));
-
-			left = ImageIO.read(getClass().getResourceAsStream("/Sprites/Buttons/left.gif"));
-			right = ImageIO.read(getClass().getResourceAsStream("/Sprites/Buttons/right.gif"));
-			enter = ImageIO.read(getClass().getResourceAsStream("/Sprites/Buttons/enter.gif"));
-
-			greendrag = ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/greendrag.gif"));
-			kat = ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/kat.gif"));
-			bluedrag = ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/bluedrag.gif"));
-			purpledrag = ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/purpledrag.gif"));
-
+			title = ImageIO.read(getClass().getResourceAsStream(
+					"/Menu/charsel.png"));
+			highlighted = ImageIO.read(getClass().getResourceAsStream(
+					"/Menu/highlighted.gif"));
+			left = ImageIO.read(getClass().getResourceAsStream(
+					"/Sprites/Buttons/left.gif"));
+			right = ImageIO.read(getClass().getResourceAsStream(
+					"/Sprites/Buttons/right.gif"));
+			enter = ImageIO.read(getClass().getResourceAsStream(
+					"/Sprites/Buttons/enter.gif"));
+			greendrag = ImageIO.read(getClass().getResourceAsStream(
+					"/Sprites/Menu/greendrag.gif"));
+			kat = ImageIO.read(getClass().getResourceAsStream(
+					"/Sprites/Menu/kat.gif"));
+			bluedrag = ImageIO.read(getClass().getResourceAsStream(
+					"/Sprites/Menu/bluedrag.gif"));
+			purpledrag = ImageIO.read(getClass().getResourceAsStream(
+					"/Sprites/Menu/purpledrag.gif"));
 			// comingsoon =
 			// ImageIO.read(getClass().getResourceAsStream("/Sprites/Menu/comingsoon.gif"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public void init() {
@@ -85,21 +79,16 @@ public class CharacterSelect extends GameState {
 		g.drawImage(left, 290, 188, null);
 		g.drawImage(right, 310, 188, null);
 		g.drawImage(enter, 80, 188, null);
-
 		g.drawImage(greendrag, 75, 130, null);
 		g.drawImage(kat, 154, 133, null);
 		g.drawImage(bluedrag, 230, 130, null);
 		g.drawImage(purpledrag, 315, 130, null);
-
 		g.setColor(versionColor);
 		g.setFont(fontInfo);
 		// g.drawString("Open Beta " + Game.version, 118, 80);
-
 		g.drawString("Choose", 83, 210);
 		g.drawString("Select", 291, 210);
-
 		g.setFont(font);
-
 		for (int i = 0; i < options.length; i++) {
 			if (i == currentChoice && i == 0) {
 				g.setColor(Color.BLACK);
@@ -125,36 +114,33 @@ public class CharacterSelect extends GameState {
 			Player.path = "/Sprites/Player/Greendrag/playersprites.gif";
 			FireBall.path = "/Sprites/Player/fireball.gif";
 			gsm.setState(1);
-			//JukeBox.stop("menuMusic");
+			// JukeBox.stop("menuMusic");
 		}
 		if (currentChoice == 1) {
 			Player.path = "/Sprites/Player/Kat/playersprites.gif";
 			FireBall.path = "/Sprites/Player/Kat/hairball.gif";
 			gsm.setState(1);
-			//JukeBox.stop("menuMusic");
+			// JukeBox.stop("menuMusic");
 		}
 		if (currentChoice == 2) {
 			Player.path = "/Sprites/Player/Bluedrag/playersprites.gif";
 			FireBall.path = "/Sprites/Player/fireball.gif";
 			gsm.setState(1);
-			//JukeBox.stop("menuMusic");
+			// JukeBox.stop("menuMusic");
 		}
 		if (currentChoice == 3) {
 			Player.path = "/Sprites/Player/Purpledrag/playersprites.gif";
 			FireBall.path = "/Sprites/Player/fireball.gif";
 			gsm.setState(1);
-			
-			//JukeBox.stop("menuMusic");
+			// JukeBox.stop("menuMusic");
 		}
 	}
 
 	public void handleInput() {
-
 		/*
 		 * if (Keys.isPressed(Keys.ENTER) && currentChoice == 1) {
 		 * sfx.get("bump").play(); return; }
 		 */
-
 		if (Keys.isPressed(Keys.ENTER) /* && currentChoice != 1 */) {
 			sfx.get("menuselect").play();
 			System.out.println("Handled Enter");
