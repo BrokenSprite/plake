2c6546683aaec6ab1e8fad00811e48332529f800
max.graman <max.graman@10.115.41.63>
2015-02-25 13:34:22 -0500

    -- Fucked shit up.
    Signed-off-by: max.graman <max.graman@10.115.41.63>

--- Plake/src/com/plake/gamestate/GameStateManager.java
+++ Plake/src/com/plake/gamestate/GameStateManager.java
@@ -17,7 +17,7 @@ public class GameStateManager {
 	private GameState[] gameStates;
 	private int currentState;
 
-	public static final int NUMGAMESTATES = 12;
+	public static final int NUMGAMESTATES = 13;
 
 	public static final int MENUSTATE = 0;
 	public static final int LEVEL1STATE = 1;
@@ -29,8 +29,10 @@ public class GameStateManager {
 	public static final int CHARSEL = 7;
 	public static final int LEVELCOMP = 8;
 	public static final int LEVEL5STATE = 9;
+
 	public static final int W2_LEVEL1STATE = 10;
 	public static final int WORLDONEMAP = 11;
+	public static final int LOADINGSTATE = 12;
 
 	public boolean l1beat, l2beat, l3beat, l4beat, l5beat;
 	public boolean w1beat;
@@ -52,31 +54,18 @@ public class GameStateManager {
 
 	}
 
-	/*public void nextLevel() {
-		LevelCompletedState.eDead = 0;
-		LevelCompletedState.score = 0;
-
-		if (l1beat) {
-			l1beat = false;
-			setState(4);
-		} else if (l2beat) {
-			l2beat = false;
-			setState(5);
-		} else if (l3beat) {
-			l3beat = false;
-			setState(6);
-		} else if (l4beat) {
-			l4beat = false;
-			setState(9);
-		} else if (l5beat) {
-			l5beat = false;
-			setState(10);
-		} else if (w2_l1beat) {
-			w2_l1beat = false;
-			setState(0);
-		}
-
-	}*/
+	/*
+	 * public void nextLevel() { LevelCompletedState.eDead = 0;
+	 * LevelCompletedState.score = 0;
+	 * 
+	 * if (l1beat) { l1beat = false; setState(4); } else if (l2beat) { l2beat =
+	 * false; setState(5); } else if (l3beat) { l3beat = false; setState(6); }
+	 * else if (l4beat) { l4beat = false; setState(9); } else if (l5beat) {
+	 * l5beat = false; setState(10); } else if (w2_l1beat) { w2_l1beat = false;
+	 * setState(0); }
+	 * 
+	 * }
+	 */
 
 	public int getCurrentState() {
 		return currentState;
---
+++ Plake/src/com/plake/gamestate/LoadingState.java
@@ -0,0 +1,36 @@
+package com.plake.gamestate;
+
+import java.awt.Graphics2D;
+
+public class LoadingState extends GameState {
+
+	public LoadingState(GameStateManager gsm) {
+		super(gsm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		System.out.println("LOADED");
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
